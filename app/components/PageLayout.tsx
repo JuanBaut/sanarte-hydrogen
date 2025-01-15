16da8b12d59d8dd4cb61bfb0a1126260b47dd89f
JuanBaut <deserd@protonmail.com>
2025-01-15 18:35:54 -0500

    removed older aside approach from boilerplate

--- app/components/PageLayout.tsx
+++ app/components/PageLayout.tsx
@@ -1,14 +1,12 @@
-import { Await, Link } from "@remix-run/react";
-import { Suspense, useId } from "react";
+import { Link } from "@remix-run/react";
+import { useId } from "react";
 import type {
   CartApiQueryFragment,
   FooterQuery,
   HeaderQuery,
 } from "storefrontapi.generated";
-import { Aside } from "~/components/Aside";
-import { CartMain } from "~/components/cart/CartMain";
 import { Footer } from "~/components/Footer";
-import { Header, HeaderMenu } from "~/components/Header";
+import { Header } from "~/components/Header";
 import {
   SEARCH_ENDPOINT,
   SearchFormPredictive,
@@ -33,10 +31,7 @@ export function PageLayout({
   publicStoreDomain,
 }: PageLayoutProps) {
   return (
-    <Aside.Provider>
-      {/*<CartAside cart={cart} />*/}
-      {/*<SearchAside />*/}
-      {/*<MobileMenuAside header={header} publicStoreDomain={publicStoreDomain} />*/}
+    <div>
       {header && (
         <Header
           header={header}
@@ -51,124 +46,86 @@ export function PageLayout({
         header={header}
         publicStoreDomain={publicStoreDomain}
       />
-    </Aside.Provider>
-  );
-}
-
-function CartAside({ cart }: { cart: PageLayoutProps["cart"] }) {
-  return (
-    <Aside type="cart" heading="CART">
-      <Suspense fallback={<p>Cargando...</p>}>
-        <Await resolve={cart}>
-          {(cart) => {
-            return <CartMain cart={cart} layout="aside" />;
-          }}
-        </Await>
-      </Suspense>
-    </Aside>
+    </div>
   );
 }
 
 function SearchAside() {
   const queriesDatalistId = useId();
   return (
-    <Aside type="search" heading="SEARCH">
-      <div className="predictive-search">
-        <br />
-        <SearchFormPredictive>
-          {({ fetchResults, goToSearch, inputRef }) => (
+    <div className="predictive-search">
+      <br />
+      <SearchFormPredictive>
+        {({ fetchResults, goToSearch, inputRef }) => (
+          <>
+            <input
+              name="q"
+              onChange={fetchResults}
+              onFocus={fetchResults}
+              placeholder="Search"
+              ref={inputRef}
+              type="search"
+              list={queriesDatalistId}
+            />
+            &nbsp;
+            <button onClick={goToSearch}>Search</button>
+          </>
+        )}
+      </SearchFormPredictive>
+
+      <SearchResultsPredictive>
+        {({ items, total, term, state, closeSearch }) => {
+          const { articles, collections, pages, products, queries } = items;
+
+          if (state === "loading" && term.current) {
+            return <div>Cargando...</div>;
+          }
+
+          if (!total) {
+            return <SearchResultsPredictive.Empty term={term} />;
+          }
+
+          return (
             <>
-              <input
-                name="q"
-                onChange={fetchResults}
-                onFocus={fetchResults}
-                placeholder="Search"
-                ref={inputRef}
-                type="search"
-                list={queriesDatalistId}
+              <SearchResultsPredictive.Queries
+                queries={queries}
+                queriesDatalistId={queriesDatalistId}
               />
-              &nbsp;
-              <button onClick={goToSearch}>Search</button>
+              <SearchResultsPredictive.Products
+                products={products}
+                closeSearch={closeSearch}
+                term={term}
+              />
+              <SearchResultsPredictive.Collections
+                collections={collections}
+                closeSearch={closeSearch}
+                term={term}
+              />
+              <SearchResultsPredictive.Pages
+                pages={pages}
+                closeSearch={closeSearch}
+                term={term}
+              />
+              <SearchResultsPredictive.Articles
+                articles={articles}
+                closeSearch={closeSearch}
+                term={term}
+              />
+              {term.current && total ? (
+                <Link
+                  onClick={closeSearch}
+                  to={`${SEARCH_ENDPOINT}?q=${term.current}`}
+                >
+                  <p>
+                    View all results for <q>{term.current}</q>
+                    &nbsp; →
+                  </p>
+                </Link>
+              ) : null}
             </>
-          )}
-        </SearchFormPredictive>
-
-        <SearchResultsPredictive>
-          {({ items, total, term, state, closeSearch }) => {
-            const { articles, collections, pages, products, queries } = items;
-
-            if (state === "loading" && term.current) {
-              return <div>Cargando...</div>;
-            }
-
-            if (!total) {
-              return <SearchResultsPredictive.Empty term={term} />;
-            }
-
-            return (
-              <>
-                <SearchResultsPredictive.Queries
-                  queries={queries}
-                  queriesDatalistId={queriesDatalistId}
-                />
-                <SearchResultsPredictive.Products
-                  products={products}
-                  closeSearch={closeSearch}
-                  term={term}
-                />
-                <SearchResultsPredictive.Collections
-                  collections={collections}
-                  closeSearch={closeSearch}
-                  term={term}
-                />
-                <SearchResultsPredictive.Pages
-                  pages={pages}
-                  closeSearch={closeSearch}
-                  term={term}
-                />
-                <SearchResultsPredictive.Articles
-                  articles={articles}
-                  closeSearch={closeSearch}
-                  term={term}
-                />
-                {term.current && total ? (
-                  <Link
-                    onClick={closeSearch}
-                    to={`${SEARCH_ENDPOINT}?q=${term.current}`}
-                  >
-                    <p>
-                      View all results for <q>{term.current}</q>
-                      &nbsp; →
-                    </p>
-                  </Link>
-                ) : null}
-              </>
-            );
-          }}
-        </SearchResultsPredictive>
-      </div>
-    </Aside>
-  );
-}
-
-function MobileMenuAside({
-  header,
-  publicStoreDomain,
-}: {
-  header: PageLayoutProps["header"];
-  publicStoreDomain: PageLayoutProps["publicStoreDomain"];
-}) {
-  return (
-    header.menu &&
-    header.shop.primaryDomain?.url && (
-      <Aside type="mobile" heading="MENU">
-        <HeaderMenu
-          menu={header.menu}
-          viewport="mobile"
-          primaryDomainUrl={header.shop.primaryDomain.url}
-          publicStoreDomain={publicStoreDomain}
-        />
-      </Aside>
-    )
+          );
+        }}
+      </SearchResultsPredictive>
+    </div>
   );
 }
